9fe5e39fcc87dbe2d0cd2a58f4b93d9d4c6c2b88
Yogesh <Yogesh2591@gmail.com>
2018-08-18 17:39:15 +0530

    Updated Query Tool Widget

--- src/app/widgets/query/query.component.ts
+++ src/app/widgets/query/query.component.ts
@@ -1,9 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-
-
+import {Globals} from '../../globals'
+import { HttpClient } from "@angular/common/http";
 export interface select {
-  value: string;
-  label: string;
+  value: any;
+  label: any;
 }
 
 
@@ -14,12 +14,55 @@ export interface select {
 })
 export class QueryComponent implements OnInit {
 
-  constructor() { }
-
-  layers:select[] = [  ]
-  fields:select[] = [   ]
+  constructor(
+    private globals :Globals,
+    private http: HttpClient
+  ) { 
+    this.map = this.globals.map
+  }
+  map:any;
+  layers:select[] = [ ]
+  fields:select[] = [ ]
 
   ngOnInit() {
+  }
+  ngAfterViewInit() {
+    let layers = this.map.getLayers().getArray();
+    for (let i = 0; i < layers.length; i++) {
+      const layer = layers[i];
+      let id = layer.get('id');
+      if(id!= undefined && id!= 0 ){
+        this.layers.push( {label:layer.get('title'),value:id} );
+      }
+    }
+  }
+  onLayerChange(e):void{
+    this.fields = [];
+    let fields = this.fields;
+    let layers = this.map.getLayers().getArray().filter(function(layer){
+      return layer.get('id') == e.value;
+    });
+   
+     let featureUrl = "http://192.168.1.14:6600/geoserver/wfs?version=1.3.0&request=describeFeatureType&outputFormat=application/json&service=WFS&typeName="+ layers[0].getSource().getParams().LAYERS ;
+     this.http.get(featureUrl).subscribe(
+      (res:any)=>{
+        let response = res;
+        if(response.featureTypes[0].properties.length >0 )
+        {
+          response.featureTypes[0].properties.forEach(function(attributes){
+           if(attributes.name != 'geom'){
+            fields.push({
+              label : attributes.name.toUpperCase(),
+              value : attributes.name
+            });
+          }
+           
+          });
+          
+        }
+       },
+      (error)=>{ console.log(error)  }
+    )
   }
 
 }
